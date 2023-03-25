bf70b900c96c2e7452613e02728ef56c716c47fd
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-03-26 01:13:02 +0300

    add svg file to mainpage and change the style

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+import tranlsateSVG from "../public/translate.svg";
+import rocketSVG from "../public/rocket.svg";
+import piggySVG from "../public/piggy.svg";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="container mt-52 mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-3">
+        <div>
+          <Image
+            src={tranlsateSVG}
+            alt="translate"
+            width={150}
+            height={150}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <Image
+            src={rocketSVG}
+            alt="rocket"
+            width={150}
+            height={150}
+            className="mx-auto"
+          />
+        </div>
+        <div>
+          <Image
+            src={piggySVG}
+            alt="piggy"
+            width={150}
+            height={150}
+            className="mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
