b0cdc072beb09c12b102edf534c2184f7033110e
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-03-26 06:47:23 +0300

    crate getaprice page

---
+++ app/getaprice/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GetaPrice = () => {
+  return (
+    <div>
+      <div className="w-3/5 h-3/5 grid grid-cols-1 sm-grid-col-3 mx-auto">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default GetaPrice;
