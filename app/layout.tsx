802334603da874f82a25836e5d480f9f2d80dd91
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-03-27 04:13:53 +0300

    add loading page and change the style

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="linear-bg">
+      <body className="linear-bg appear">
         <div className="container">
           <div className="grid xl:grid-cols-2 sm:grid-cols-2 gap-4 mt-20">
             <div className="text-3xl font-bold mx-10 flex md:mx-20 text-center cursor-pointer hover:scale-105 transition-all">
@@ -59,7 +59,7 @@ export default function RootLayout({
         {children}
         <footer className="h-28 mt-40 grid">
           <div className="mt-20 bg-black grid text-white w-auto h-full text-center text-base content-center">
-            <h1>ONLINE TRANSLATE © 2021. Tüm hakları saklıdır. | </h1>
+            <h1>ONLINE TRANSLATE © 2023. Tüm hakları saklıdır. | </h1>
           </div>
         </footer>
       </body>
---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+const loading = () => {
+  return (
+    <div className="container grid w-full h-3/6 place-items-center mx-auto">
+      <div className="flex flex-col items-center justify-center mainPageText text-3xl">
+        Yükleniyor...
+      </div>
+    </div>
+  );
+};
+export default loading;
