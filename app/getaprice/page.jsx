1fc1a0f0b95bdaed0fa0dc20fe541c96713ada9e
Darvan Ceyhan <115955030+darwanceyhan@users.noreply.github.com>
2023-03-30 21:37:14 +0300

    change cards and styling

--- app/getaprice/page.jsx
+++ app/getaprice/page.jsx
@@ -82,8 +82,10 @@ function GetaPrice() {
           </h1>
           <p
             className={`${
-              level[0] ? "text-white" : "text-black"
-            } "text-sm sm:text-base text-center"
+              level[0]
+                ? "text-white text-sm sm:text-base text-center"
+                : "text-black text-sm sm:text-base text-center"
+            }
            }`}
           >
             Dosyanızı yükleyin ve hangi dilde çevrileceğini seçin.
@@ -229,40 +231,71 @@ function GetaPrice() {
       {
         //level 2 , cards
         level[1] ? (
-          <div className="w-4/5 h-3/5 grid grid-cols-1 mx-auto mt-52 sm:mt-20 place-content-center">
-            <div className="mx-auto grid grid-cols-2 mt-20">
-              <div className="grid bg-neutral-300 w-3/6 mx-auto items-center p-4 pb-8 sm:p-4 lg:p-8 dark:bg-gray-900">
-                <span className="text-sm text-center font-semibold">Basic</span>
-                <p className="text-5xl font-bold text-center">39€</p>
-                <ul className="list-disc">
-                  <li className="text-md font-semibold text-center mt-10">
-                    Lorem ipsum dolor sit amet.
-                  </li>
-                  <li className="text-md font-semibold text-center mt-10">
-                    Lorem ipsum dolor sit amet.
-                  </li>
-                  <li className="text-md font-semibold text-center mt-10">
-                    Lorem ipsum dolor sit amet.
-                  </li>
-                  <li className="text-md font-semibold text-center mt-10">
-                    Lorem ipsum dolor sit amet.
-                  </li>
-                </ul>
-                <button className="px-8 py-3 mt-6 text-lg font-semibold border rounded sm:mt-12 dark:border-gray-700">
-                  Sign up
-                </button>
-              </div>
-              <div className="grid bg-neutral-300 w-3/6 items-center mx-auto p-4 pb-8 text-center rounded-md sm:p-8 lg:p-8 dark:bg-violet-400 dark:text-gray-900">
-                <span className="text-sm font-semibold">Advanced</span>
-                <p className="text-5xl font-bold">89€</p>
-                <p className="font-semibold">
-                  Nemo deserunt possimus quo provident recusandae! Dolores qui
-                  architecto omnis pariatur eos voluptatibus sequi cum, non
-                  nesciunt aspernatur a?
-                </p>
-                <button className="px-8 py-3 mt-6 text-lg font-semibold rounded sm:mt-12 dark:bg-gray-800 dark:text-gray-50">
-                  Sign up
-                </button>
+          <div className="w-4/5 h-3/5 grid grid-cols-1 mx-auto mt-52 sm:mt-40 place-content-center">
+            <div class="container m-auto">
+              <div class="flex flex-wrap items-center justify-center w-full text-center">
+                <div class="w-full md:w-1/2 lg:w-1/3 p-4 ">
+                  <div class="flex flex-col rounded border-2 cardBoxShadow">
+                    <div class="py-5 text-black bg-white">
+                      <h3>Basic</h3>
+                      <p class="text-5xl font-bold">
+                        $19.<span class="text-3xl">95</span>
+                      </p>
+                      <p class="text-xs uppercase">Per Month</p>
+                    </div>
+                    <div class="py-5 bg-black text-white rounded-b">
+                      <p>Feature of the plan</p>
+                      <p>Another feature plan feature</p>
+                      <p>Yet another plan feature</p>
+                      <button class="px-5 py-2 mt-5 uppercase rounded bg-white text-black font-semibold hover:bg-blue-900 hover:text-white">
+                        Get Started
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="w-full md:w-1/2 lg:w-1/3 p-4">
+                  <div class="flex flex-col rounded">
+                    <div class="py-7 bg-blue-700 text-white rounded-t">
+                      <h2 class="uppercase text-yellow-300 font-extrabold">
+                        Most Popular
+                      </h2>
+                      <h3>Standard</h3>
+                      <p class="text-5xl font-bold">
+                        $49.<span class="text-3xl">95</span>
+                      </p>
+                      <p class="text-xs uppercase">Per Month</p>
+                    </div>
+                    <div>
+                      <div class="pt-1 pb-7 bg-blue-700 text-white rounded-b">
+                        <p>Feature of the plan</p>
+                        <p>Another feature plan feature</p>
+                        <p>Yet another plan feature</p>
+                        <button class="px-5 py-2 mt-5 uppercase rounded bg-yellow-300 text-blue-700 font-semibold hover:bg-blue-900 hover:text-white">
+                          Get Started
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="w-full md:w-1/2 lg:w-1/3 p-4">
+                  <div class="flex flex-col rounded border-2 border-blue-700 bg-blue-700">
+                    <div class="py-5 text-blue-700 bg-white">
+                      <h3>Advanced</h3>
+                      <p class="text-5xl font-bold">
+                        $99.<span class="text-3xl">95</span>
+                      </p>
+                      <p class="text-xs uppercase">Per Month</p>
+                    </div>
+                    <div class="py-5 bg-blue-700 text-white rounded-b">
+                      <p>Feature of the plan</p>
+                      <p>Another feature plan feature</p>
+                      <p>Yet another plan feature</p>
+                      <button class="px-5 py-2 mt-5 uppercase rounded bg-white text-blue-700 font-semibold hover:bg-blue-900 hover:text-white">
+                        Get Started
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
